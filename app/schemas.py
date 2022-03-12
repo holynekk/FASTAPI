ec46d64575029962c23aa599f35f1bb22e67ac5d
holynekk <e238109@metu.edu.tr>
2022-03-12 23:50:01 +0300

    vote system database

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
+from pydantic.types import conint
 
 
 class UserCreate(BaseModel):
@@ -41,3 +42,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+class Vote(BaseModel):
+    post_id: int
+    # Restrict dir to be less than or equal to 1
+    dir: conint(le=1)
