af3782b5d9f7402b866f089e824727907adf55e1
holynekk <e238109@metu.edu.tr>
2022-03-12 14:43:29 +0300

    endpoint tags for documentations

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,7 +4,7 @@ from typing import List
 from .. import models, schemas
 from ..database import get_db
 
-router = APIRouter(prefix="/posts")
+router = APIRouter(prefix="/posts", tags=['Posts'])
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
